81debf321dcc60fe7fb82426ffb3598277f37962
YoyoTech1B <yoyoblue30@gmail.com>
2026-07-31 12:59:47 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -14,6 +14,22 @@ self.addEventListener("install", event => {
     );
 });
 
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys.map(key => {
+                    if (key !== CACHE_NAME) {
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+});
+
+
 self.addEventListener("fetch", event => {
     event.respondWith(
         caches.match(event.request)
