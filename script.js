1fd7df86c5eeb1f11245a3a3968ee65ce1f0099d
YoyoTech1B <yoyoblue30@gmail.com>
2026-08-05 23:01:09 +0300

    Import signOut from Firebase Auth

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ import {
     getAuth,
     signInWithEmailAndPassword,
     createUserWithEmailAndPassword
+   import { signOut } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js";
 } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js";
 
 const firebaseConfig = {
